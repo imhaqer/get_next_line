c37893116bf2a5a4b90ff5d36a792368dbfb4b0a
Hager Hamdan <93748219+imhaqer@users.noreply.github.com>
2024-06-21 15:08:11 +0300

    added 'ft_append', 'ft_line & 'left_line' functions

--- get_next_line.c
+++ get_next_line.c
@@ -6,13 +6,64 @@
 /*   By: hahamdan <hahamdan@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/20 15:18:20 by hahamdan          #+#    #+#             */
-/*   Updated: 2024/06/21 14:19:52 by hahamdan         ###   ########.fr       */
+/*   Updated: 2024/06/21 15:03:14 by hahamdan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char    *read_file(int fd, char *file_content) // static_var
+char    *ft_append(char *buffer, char *stash)
+{
+    char *temp;
+
+    temp = strjoin(buffer, stash);
+    free(buffer);
+    return (temp);
+}
+
+char *ft_line(char *buffer)
+{
+    char *line;
+    
+    int i = 0;
+    while(buffer[i] && buffer[i] != '\n')
+        i++;
+    
+    line = calloc(i + 2, sizeof(char));
+
+    i = 0;
+    while (buffer[i] && buffer[i] != '\n')
+    {
+        line[i] = buffer[i];
+        i++;
+    }
+    if (buffer[i] == '\n')
+    {
+        line[i++] = '\n';
+    }
+    return (line);
+}
+char    *remaining_line(char *buffer)  // the same buffer
+{
+    char *remaining;
+    int i = 0; 
+    int j = 0;
+
+    while(buffer[i] && buffer[i] != '\n')
+        i++;
+    
+    remaining = calloc(strlen(buffer) - i + 1, sizeof(char));
+    i++; // skipping '\n'
+    while(buffer[i])
+    {
+        remaining[j] = buffer[i];
+        j++;
+        i++;
+    }
+    free(buffer);
+    return (remaining);
+}
+char    *read_file(int fd, char *file_content)
 {
     char *buffer;
     int bytesRead;
@@ -24,13 +75,13 @@ char    *read_file(int fd, char *file_content) // static_var
 
     while (bytesRead = (fd, buffer, BUFFER_SIZE))
     {
-        if (bytesRead == -1)
-        {
-            free(buffer);
-            return (NULL);
-        }
+        // if (bytesRead == -1)
+        // {
+        //     free(buffer);
+        //     return (NULL);
+        // }
         buffer[bytesRead] = '\0';
-        file_content = ft_free(file_content, buffer);
+        file_content = ft_append(buffer, file_content);
         if (ft_strchr(buffer, '\n'))
             break ;
     }
