8de94d2a75060c4cca1864f7659929db8f44e341
Hager Hamdan <93748219+imhaqer@users.noreply.github.com>
2024-06-24 12:52:33 +0300

    changed helper functions

--- get_next_line.h
+++ get_next_line.h
@@ -6,27 +6,29 @@
 /*   By: hahamdan <hahamdan@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/20 15:15:31 by hahamdan          #+#    #+#             */
-/*   Updated: 2024/06/21 15:44:45 by hahamdan         ###   ########.fr       */
+/*   Updated: 2024/06/21 16:34:14 by hahamdan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-#define BUFFER_SIZE 42
+#define BUFFER_SIZE 4
 
 # include <stdlib.h>
 # include <sys/types.h>
 # include <sys/uio.h>
 # include <unistd.h>
-# include <strings.h>
+// # include <string.h>
+# include  <fcntl.h>
 
 
 char    *get_next_line(int fd);
-char    *read_file(int fd, char *file_content);
-char    *ft_append(char *buffer, char *stash);
-char    *ft_line(char *buffer);
-char    *left_line(char *buffer);
+char	*ft_strchr(char const *str, int c);
 
+char	*ft_strjoin(char const *s1, char const *s2);
+size_t	ft_strlen(const char *c);
+void	ft_bzero(void *str, size_t len);
+void	*ft_calloc(size_t count, size_t size);
 
 #endif
